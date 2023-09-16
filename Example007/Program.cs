75c907d216722c0915d5768c3e2609909e3f5c6b
dmstrigin <dmstrigin@yandex.ru>
2023-09-16 17:13:39 +0300

    add Example007

--- Example007/Program.cs
+++ Example007/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.SetCursorPosition(10, 4);
+Console.WriteLine("X");
+
+int xa = 20;
+int ya = 1;
+int xb = 1;
+int yb = 30;
+int xc = 40;
+int yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("X");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("X");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("X");
